4ad0507af2683adf50ec0f25020c8dd17ffab98a
Evgeny Potanin <builder.nn@gmail.com>
2025-03-25 13:50:10 +0300

    add custom validator functions

--- src/app/forms/forms.component.ts
+++ src/app/forms/forms.component.ts
@@ -1,5 +1,16 @@
 import { Component } from '@angular/core';
-import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { AbstractControl, FormControl, FormGroup, ReactiveFormsModule, ValidationErrors, ValidatorFn, Validators } from '@angular/forms';
+
+export function checkRegExp(regExp: RegExp): ValidatorFn {
+  return (control: AbstractControl): ValidationErrors | null => {
+    const forbidden = regExp.test(control.value);
+    return !forbidden ? {forbiddenValue: {value: control.value}} : null;
+  }
+}
+
+export const conformPassword: ValidatorFn = (control: AbstractControl): ValidationErrors | null => {
+  return control.value.password_one === control.value.password_two ? null : { PasswordDoNotMatch: true };
+}
 
 @Component({
   selector: 'app-forms',
@@ -16,6 +27,14 @@ export class FormsComponent {
     email: new FormControl('', [Validators.required, Validators.email]),
     password: new FormControl('', Validators.required),
   })
+
+  public validatorsForm = new FormGroup({
+    mail: new FormControl('', checkRegExp(/^[\w\.]+@([\w-]+\.)+[\w-]{2,4}$/g)),
+    password_one: new FormControl(''),
+    password_two: new FormControl(''),
+  },
+  conformPassword
+  )
 
   handlerValue() {
     if(this.myForm.valid) {
